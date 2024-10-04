9ec76ef73ed30790a5f136cf444809ae8e18de6b
vinoles <felipe.vinoles@gmail.com>
2024-10-04 16:23:02 +0200

    remove imports unused

--- src/feeds/test/feed.base.service.testing.module.ts
+++ src/feeds/test/feed.base.service.testing.module.ts
@@ -1,9 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { FeedsController } from '../feeds.controller';
 import { FeedsService } from '../feeds.service';
-import { Feed, FeedSchema } from '../schemas/feed.schema';
-import { MongooseModule, getModelToken } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+import { getModelToken } from '@nestjs/mongoose';
 import { FakeFeedsFactory } from './fake.feeds.factory';
 
 export const createTestingFeedServiceModule =
