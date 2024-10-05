7e831b7015b3acb9a0f83e096a81a24864729fc3
vinoles <felipe.vinoles@gmail.com>
2024-10-05 18:07:37 +0200

    remove interfaces unused

--- src/feeds/scraper/ScraperPageInterface.ts
+++ src/feeds/scraper/ScraperPageInterface.ts
@@ -8,16 +8,6 @@ export interface CategoryPage {
   categoryPage: string;
 }
 
-export interface PageCountryArticle {
-  title: string;
-  url: string;
-}
-
-export interface CategoryPageCountryArticles {
-  categoryPage: string;
-  articles: PageCountryArticle[];
-}
-
 export interface PageArticle {
   title: string;
   url: string;
